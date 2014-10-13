a9730e578e734b24f6eb94693ad380b596c03ec0
Treemo <treemo@hotmail.fr>
2014-10-13 18:35:04 +0000

    Add debug log

--- Module.py
+++ Module.py
@@ -1,5 +1,8 @@
+from core import Log
+
 import urllib
 import urllib.request
+
 
 class Module:
     def __init__(self):
@@ -25,6 +28,8 @@ class Module:
         request.get_method = lambda : method.upper()
         request.add_header('User-Agent','Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)')
         request.add_header('Referer', url)
+
+        Log.debug('http_lib : %s %s' % (method, url))
         result = urllib.request.urlopen(request, timeout=self.timeout)
 
         if 'return_object' in options and options['return_object']:
